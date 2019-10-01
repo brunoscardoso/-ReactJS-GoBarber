002339bc15af5aa56a4d86de3253d4ccfd6cb5d0
brunoscardoso <bruno.soarescardoso@outlook.com>
2019-10-01 20:32:16 -0300

    Initial Store

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Email inválido')
+    .required('O email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'Senha precisa ter pelo menos 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
